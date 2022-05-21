4080a7a15dd0939323369ab1931363aca7267f43
Paul IDO <paulido92@gmail.com>
2022-05-21 19:46:10 +0000

    refact : change project structure

--- setup.py
+++ setup.py
@@ -4,15 +4,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
-    version='dev',
-    author='IDO Paul',
-    author_email='paulido92mail.com',
-    description='toolbox is a package for machine learning. it is used to generate randoms problems of regression and classification',
+    name="toolbox",
+    version="0.0.1",
+    author="IDO PAUL",
+    author_email="paulido92@gmail.com",
+    description="toolbox is python machine learning package. It is serve for generate randoms regression and classification problems",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_dir='https://github.com/paulido/toolbox',
-    license='MIT',
-    packages=['toolbox'],
-    install_requires=['requests'],
+    url="https://github.com/paulido/toolbox",
+    project_urls={
+        "Bug Tracker": "https://github.com/ido/toolbox/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
